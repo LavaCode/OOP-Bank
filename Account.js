37dc69cdbdda1298b94c9428f5a9e55d05297148
LavaCode <kasper.zegel@gmail.com>
2021-02-18 17:04:16 +0100

    completed inheritance, test runs as supposed

--- Account.js
+++ Account.js
@@ -1,2 +1,34 @@
-// Export your class here as module.exports = MyClass
-module.exports = null;
+class Account {
+    constructor(name, balance, creditLimit) {
+        this.name = name;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+    }
+
+    makeDeposit(amount) {
+        if (amount < 0) {
+            return `Uhm, that's impossible. Balance: ${this.balance}`;
+        } else {
+            return `Your new balance is ${(this.balance += amount)}`;
+        }
+    }
+
+    makeWithdrawal(amount) {
+        if (amount > this.balance) {
+            return `Insufficient funds! Balance: ${this.balance}`;
+        } else if (amount > 5000) {
+            return `That's above the daily limit. Balance is: ${this.balance}`;
+        } else {
+           return `New balance: ${(this.balance -= amount)}`;
+        }
+    }
+
+}
+
+const accountHolder = new Account('Rein', 500, 2000);
+
+console.log(`hi my name is ${accountHolder.name}, today I have a balance of ${accountHolder.balance} and a credit limit of ${accountHolder.creditLimit}`)
+console.log(accountHolder.makeDeposit(1000));
+
+
+module.exports = Account;
--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,2 +1,40 @@
-// Export your class here as module.exports = MyClass
-module.exports = null;
+const Account = require("./Account");
+
+class BusinessAccount extends Account {
+    constructor(name, balance, creditLimit, sepaPermission) {
+        super(name, balance, creditLimit);
+        this.sepaPermission = sepaPermission;
+    }
+
+    makeDeposit(amount) {
+        if (amount < 0) {
+            return `Uhm, that's impossible. Balance: ${this.balance}`;
+        } else {
+            return `Your new balance is ${(this.balance += amount)}`
+        }
+    }
+
+    makeWithdrawal(amount) {
+        if (amount > this.balance || amount > (this.creditLimit + this.balance)|| amount > 20000) {
+            return `You're exceeding limits. Balance: ${this.balance}`;
+        } else {
+            return `New balance: ${(this.balance -= amount)}`;
+        }
+    }
+
+    sepaInvoice(amount) {
+        if (this.sepaPermission) {
+            if (amount < 0) {
+                return `Uhm, that's a negative. Balance: ${this.balance}`;
+            } else {
+                this.balance += (amount * 0.99)
+                return `New balance: ${this.balance}`;
+            }
+        }
+        return `You don't have permission. Balance: ${this.balance}`;
+    }
+}
+
+const company = new BusinessAccount();
+
+module.exports = BusinessAccount ;
